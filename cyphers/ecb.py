51e3431e89cbd6bc1ac45e73f90d86cd24d318e1
Gennady Sytnik <gsytnik@calpoly.edu>
2022-09-29 10:23:51 -0700

    ecb updates

--- cyphers/ecb.py
+++ cyphers/ecb.py
@@ -1,9 +1,21 @@
-from cryptography.fernet import Fernet
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 def ecb(infile: str, outfile: str):
 	print("ecb")
-	key = Fernet.generate_key()
+	key = os.urandom(16)
+	iv = os.urandom(16)
+	cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+	encryptor = cipher.encryptor()
 
-	with open(infile) as
+	# enc = encryptor.update() + encryptor.finalize()
+
+	with open(infile, 'rb') as f:
+
+		enc = encryptor.update(f.read(16)) + encryptor.finalize()
+
+		with open(f'outputs/{outfile}', 'wb') as f2:
+			f2.write(enc)
+
 
 	return
