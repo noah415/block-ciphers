ff319b320983b6dee61c20ee9bb240280a72b3db
Gennady Sytnik <gsytnik@calpoly.edu>
2022-09-29 10:32:46 -0700

    updates

--- cyphers/ecb.py
+++ cyphers/ecb.py
@@ -8,14 +8,23 @@ def ecb(infile: str, outfile: str):
 	cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
 	encryptor = cipher.encryptor()
 
-	# enc = encryptor.update() + encryptor.finalize()
-
-	with open(infile, 'rb') as f:
-
-		enc = encryptor.update(f.read(16)) + encryptor.finalize()
-
-		with open(f'outputs/{outfile}', 'wb') as f2:
-			f2.write(enc)
-
+	_encrypt_file(infile, outfile, encryptor)
 
 	return
+
+def _encrypt_file(infile: str, outfile: str, encryptor: Cipher):
+
+	f = open(infile, 'rb')
+	enc_out = b''
+
+	while True:
+
+		data = f.read(16)
+		if not data:
+			break
+
+		enc_out += encryptor.update(data)
+
+	with open(f'outputs/{outfile}', 'wb') as f2:
+		f2.write(enc_out)
+
