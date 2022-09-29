0f02244cdf47eaf55dc302ea61125ea0ce9ccd9a
Gennady Sytnik <gsytnik@calpoly.edu>
2022-09-29 10:40:29 -0700

    constant fix plus ecb update

--- cyphers/ecb.py
+++ cyphers/ecb.py
@@ -1,10 +1,11 @@
 import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from constants import *
 
 def ecb(infile: str, outfile: str):
 	print("ecb")
-	key = os.urandom(16)
-	iv = os.urandom(16)
+	key = os.urandom(CHUNKSIZE)
+	iv = os.urandom(CHUNKSIZE)
 	cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
 	encryptor = cipher.encryptor()
 
@@ -15,16 +16,18 @@ def ecb(infile: str, outfile: str):
 def _encrypt_file(infile: str, outfile: str, encryptor: Cipher):
 
 	f = open(infile, 'rb')
+	header = f.read(BMPHEADER)
 	enc_out = b''
 
 	while True:
 
-		data = f.read(16)
+		data = f.read(CHUNKSIZE)
 		if not data:
 			break
 
 		enc_out += encryptor.update(data)
 
 	with open(f'outputs/{outfile}', 'wb') as f2:
+		f2.write(header)
 		f2.write(enc_out)
 
